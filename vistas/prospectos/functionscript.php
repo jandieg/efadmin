47afac9560f83db8ad5b5050693ecb7ed29198e4
daniel <danieljosebecerra@gmail.com>
2017-08-03 15:03:45 -0400

    hotfix al crear aplicante

--- vistas/prospectos/functionscript.php
+++ vistas/prospectos/functionscript.php
@@ -297,8 +297,8 @@ var setConvertirAplicante = function(){
                     if(mensaje.success == "true"){
                   //      load_miembro($("#convertir_id").val());
                   if ($("#_email").is(":checked")) {
-                    	//window.location.replace("http://"+ window.location.hostname + "/admin/miembros?id_miembro="+$("#convertir_id").val()+"&email=1");	                                        
-                    window.location.replace("http://localhost:3000/sistema/public_html/admin/miembros?id_miembro="+$("#convertir_id").val()+"&email=1");	                                        
+                    	window.location.replace("http://"+ window.location.hostname + "/admin/miembros?id_miembro="+$("#convertir_id").val()+"&email=1");	                                        
+                  
                   } else {
                     	window.location.replace("http://"+ window.location.hostname + "/admin/miembros?id_miembro="+$("#convertir_id").val());	                                        
                     //window.location.replace("http://localhost:3000/sistema/public_html/admin/miembros?id_miembro="+$("#convertir_id").val());	                                              
