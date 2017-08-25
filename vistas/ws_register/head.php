efe06b9f7d150d9c123e357ac4e4a234453b5c4f
daniel <danieljosebecerra@gmail.com>
2017-08-25 12:38:43 -0400

    enviar copia a cinthya

--- vistas/ws_register/head.php
+++ vistas/ws_register/head.php
@@ -21,8 +21,8 @@ if (isset($data)) {
 				$cuerpoMensaje="Estimado ".$member["name"].", utiliza el siguiente código para acceder a la APP de Executive Forums:<br><br> ".$code;
 				
 				$mail= new Mail();
-				$msg= $mail->enviar("Executive Forums - APP", "", "Código de acceso a App Executive Forums", $cuerpoMensaje, $data->user, TRUE);
-
+				$msg= $mail->enviar("Executive Forums - APP", "", "Código de acceso a App Executive Forums", $cuerpoMensaje, $data->user, TRUE, 'cruizds@executiveforums.com');
+				
 					 $response["success"] = "1"; 
 				     $response["data"] = $resultset['usuario'][0]; 
 				     echo json_encode($response); 
@@ -35,7 +35,7 @@ if (isset($data)) {
 							$cuerpoMensaje="Estimado ".$member["name"].", utiliza el siguiente código para acceder a la APP de Executive Forums:<br><br> ".$code;
 				
 							$mail= new Mail();
-							$msg= $mail->enviar("Executive Forums - APP", "", "Código de acceso a App Executive Forums",  $cuerpoMensaje, $data->user, TRUE);
+							$msg= $mail->enviar("Executive Forums - APP", "", "Código de acceso a App Executive Forums",  $cuerpoMensaje, $data->user, TRUE, 'cruizds@executiveforums.com');
 
 								 $response["success"] = "1"; 
 								 $response["data"] = $resultset['staff'][0]; 
