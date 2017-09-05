0be1d196891fd8c427d589cc45c98f52102c4b2e
daniel <danieljosebecerra@gmail.com>
2017-09-05 08:12:48 -0400

    hotfix login

--- vistas/login/head.php
+++ vistas/login/head.php
@@ -67,7 +67,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         redirect("miembroperfil");  
                     }else{
                     //    redirect("perfil");
-					 redirect("prospectos?_esaplicante=0");
+					 redirect("/");
                     }
 
 
